ec329fcb6c70db700a339124127f6417a581132d
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-30 18:58:21 -0400

    load feed with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,7 +1,3 @@
-
-
-
-
 const createTweetElement = (data) => {
   let $tweet = `
   <article class="tweet">  
@@ -9,7 +5,7 @@ const createTweetElement = (data) => {
       <section class="image-name">
         <div class="avatar"><img src= ${data.user.avatars}></div>
         <div class="user-name">${data.user.name}</div>
-        </section>
+      </section>
       <div>${data.user.handle}</div>
     </header> 
     <p class="tweet-text">${data.content.text}</p> 
@@ -26,13 +22,31 @@ const createTweetElement = (data) => {
   return $tweet;
 }
 
+const loadTweets = function() {
+  $.ajax({
+    method: "GET",
+    url: "/tweets"
+  })
+  .then(function(data) {
+    renderTweets(data);
+  });
+} 
+
+const loadFeed = () => {
+  $.ajax({
+    method: "GET",
+    url: "/tweets",
+  })
+  .then(function(data) {
+    $('.tweet-container').prepend(createTweetElement(data[data.length - 1]))
+  });
+}
 
 const renderTweets = (tweetData) => {
   for (const tweet of tweetData) {
     $('.tweets-container').append(createTweetElement(tweet));
   }
 }
-
 
 $(document).ready(function() {
 
@@ -66,20 +80,111 @@ $(document).ready(function() {
   $('.post-tweet').on('submit', function (event) {
     event.preventDefault();
     const input = $('.new-tweet-text').val();
+    const tweetLength = $('.new-tweet-text').val().length;
     console.log(input);
 
-    // $.ajax({
-    //   method: "POST",
-    //   action: '/tweets',
-    //   data: "field1=tweet&field2=user-info&
-    // })
-    // .then(function(data) {
-    //   renderTweets(data);
-    // })
-    // .catch(function (err) {
-    //   console.log(err);
-    // })
+    $.ajax({
+      method: "POST",
+      action: '/tweets',
+      data: input,
+    })
+    .then(function(data) {
+      renderTweets(data);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
 
   })
 
 });
+
+// const safe = function(str) {
+//   let div = document.createElement("div");
+//   div.appendChild(document.createTextNode(str));
+//   return div.innerHTML;
+// };
+
+// const renderTweets = function(data) {
+//   data.forEach(userInfo => {
+//     $(".tweets").prepend(createTweetElement(userInfo));
+//   });
+// };
+
+// const createTweetElement = function(userInfo) {
+//   const { user, content, created_at } = userInfo;
+//   const time = timeago.format(created_at, "en_US");
+//   return `<article class="tweet">
+//   <header class="handle-info">
+//     <div class="avatar-name">
+//       <img src=${user.avatars}></img>
+//       <div>${user.name}</div>
+//     </div>
+//     <div>${user.handle}</div>
+//   </header>
+//   <p class="tweet-content">${safe(content.text)}</p>
+//   <footer class="more-actions">
+//     <time>${time}</time>
+//     <div class="icons">
+//       <i class="fa-solid fa-flag"></i>
+//       <i class="fa-sharp fa-solid fa-retweet"></i>
+//       <i class="fa-solid fa-thumbs-up"></i>
+//     </div>
+//   </footer>
+//   </article>`;
+// };
+
+// const loadTweets = function() {
+//   $.ajax({
+//     method: "GET",
+//     url: "/tweets"
+//   })
+//     .then(function(data) {
+//       renderTweets(data);
+//     });
+// };
+
+// const addingToFeed = function() {
+//   $.ajax({
+//     method: "GET",
+//     url: "/tweets"
+//   })
+//     .then(function(data) {
+//       $(".tweets").prepend(createTweetElement(data[data.length - 1]));
+//     });
+// };
+
+// $(document).ready(function() {
+//   loadTweets();
+
+//   $(".tweet-form").on("submit", function(event) {
+//     event.preventDefault();
+//     const input = $("#tweet-text").serialize();
+//     const tweetLength = $("#tweet-text").val().length;
+
+//     if (tweetLength) {
+//       $(".error-message").slideUp();
+//     }
+
+//     if (tweetLength > 140) {
+//       $(".error-message").text("Tweet is too long!").slideDown();
+//       return;
+//     }
+    
+//     if (tweetLength === 0) {
+//       $(".error-message").text("Type something you clown!").slideDown();
+//       return;
+//     }
+
+//     $.ajax({
+//       method: "POST",
+//       url: "/tweets",
+//       data: input,
+//     })
+//       .then(function() {
+//         addingToFeed();
+//         $("#tweet-text").val("");
+//         $(".counter").val(140);
+//       });
+//   });
+// });
